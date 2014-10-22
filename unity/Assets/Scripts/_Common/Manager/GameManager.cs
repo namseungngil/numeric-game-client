0ee41af94bd5d8ebf955a15466d060b9a5e5ca53
sinam <sinam@minamichanBluetooth-PAN.local>
2014-10-22 21:16:15 +0900

    Create Shake

--- unity/Assets/Scripts/_Common/Manager/GameManager.cs
+++ unity/Assets/Scripts/_Common/Manager/GameManager.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public abstract class GameManager : MonoBehaviour
 {
+	// variable
+	private float cameraShake = 0;
+	private float shakeAmount = 0.7f;
+	private float decreaseFactor = 1.0f;
+
 	protected void Update ()
 	{
 		if (Application.platform == RuntimePlatform.Android) {
@@ -11,8 +16,23 @@ public abstract class GameManager : MonoBehaviour
 				AndroidBackButton ();
 			}
 		}
+
+		if (cameraShake > 0) {
+			Camera.main.transform.localPosition = Random.insideUnitSphere * shakeAmount;
+			cameraShake -= Time.deltaTime * decreaseFactor;
+
+			if (cameraShake < 0) {
+				cameraShake = 0;
+			}
+		}
 	
 	}
 
 	protected abstract void AndroidBackButton ();
+
+	protected void CameraShake (float value = 1.0f)
+	{
+		cameraShake = value;
+	}
+
 }
